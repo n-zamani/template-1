b8fe0da8d207b004cedc1b0be3722f9d2ef806d0
Niloofar Zamani <niloofar.zamani92@gmail.com>
2020-06-12 18:35:46 +0430

    newly coded

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
+import '../css/style.scss';
+import '../files/hamburger.jpg';
+import '../files/tablesetting.jpg';
+import '../files/tablesetting2.jpg';
